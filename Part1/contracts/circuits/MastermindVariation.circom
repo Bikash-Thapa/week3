0eb61de04b399a947deb79a2fe06d6ef0fc6767a
Bikash-Thapa <bikashthapa_420@yahoo.com>
2022-05-25 22:02:53 +0545

    Week3 Assignment Completed

--- Part1/contracts/circuits/MastermindVariation.circom
+++ Part1/contracts/circuits/MastermindVariation.circom
@@ -1,9 +1,115 @@
 pragma circom 2.0.0;
 
 // [assignment] implement a variation of mastermind from https://en.wikipedia.org/wiki/Mastermind_(board_game)#Variation as a circuit
+// Here, the mastermind variation used is Electronic Mastermind also called as Invicta.
+// The variation of the mastermind is implemented by taking the reference from eno-kuro's zku-final-project repository hitandblow.circom
+// Invicta uses 10 digits (from 0 to 9) as alias of colors and can use 3, 4, or 5 holes(which are number of guesses made in hitandblow.circom)
+// The below code is implemented by taking the copy of hitandblow.circom (https://github.com/enu-kuro/zku-final-project/blob/main/circuits/hitandblow.circom)
+// It is then modified to take 5 input signals for the guess instead of 4.
+
+// Circom code imported from circomlib library
+include "../../node_modules/circomlib/circuits/comparators.circom";
+include "../../node_modules/circomlib/circuits/bitify.circom";
+include "../../node_modules/circomlib/circuits/poseidon.circom";
 
 template MastermindVariation() {
 
-}
+    // Five Public inputs
+    signal input pubGuessA;
+    signal input pubGuessB;
+    signal input pubGuessC;
+    signal input pubGuessD;
+    signal input pubGuessE;
+    signal input pubNumHit;
+    signal input pubNumBlow;
+    signal input pubSolnHash;
 
-component main = MastermindVariation();
+    // Five Private inputs
+    signal input privSolnA;
+    signal input privSolnB;
+    signal input privSolnC;
+    signal input privSolnD;
+    signal input privSolnE;
+    signal input privSalt; // input signal that prevents from brute-force attacks
+
+    // Output of hash of solution made by codemaker along with additional privSalt
+    signal output solnHashOut;
+
+    var guess[5] = [pubGuessA, pubGuessB, pubGuessC, pubGuessD, pubGuessE];
+    var soln[5] =  [privSolnA, privSolnB, privSolnC, privSolnD, privSolnE];
+    var j = 0;
+    var k = 0;
+    component lessThan[10];     // 10 components declared for ensuring that 5 guesses and 5 solutions are less than 10
+    component equalGuess[10];   // 10 components declared for ensuring that all the guess solutions are unique
+    component equalSoln[10];    // 10 components declared for ensuring that all the exact corrrect solutions are unique
+    var equalIdx = 0;
+
+    // Create a constraint that the solution and guess digits are all less than 10.
+    for (j=0; j<5; j++) {
+        lessThan[j] = LessThan(4);
+        lessThan[j].in[0] <== guess[j];
+        lessThan[j].in[1] <== 10;
+        lessThan[j].out === 1;
+        lessThan[j+5] = LessThan(4);
+        lessThan[j+5].in[0] <== soln[j];
+        lessThan[j+5].in[1] <== 10;
+        lessThan[j+5].out === 1;
+        for (k=j+1; k<5; k++) {
+            // Create a constraint that the solution and guess digits are unique. no duplication.
+            equalGuess[equalIdx] = IsEqual();
+            equalGuess[equalIdx].in[0] <== guess[j];
+            equalGuess[equalIdx].in[1] <== guess[k];
+            equalGuess[equalIdx].out === 0;
+            equalSoln[equalIdx] = IsEqual();
+            equalSoln[equalIdx].in[0] <== soln[j];
+            equalSoln[equalIdx].in[1] <== soln[k];
+            equalSoln[equalIdx].out === 0;
+            equalIdx += 1;
+        }
+    }
+
+    // Count hit & blow
+    var hit = 0;
+    var blow = 0;
+    component equalHB[25];
+
+    for (j=0; j<5; j++) {
+        for (k=0; k<5; k++) {
+            equalHB[5*j+k] = IsEqual();
+            equalHB[5*j+k].in[0] <== soln[j];
+            equalHB[5*j+k].in[1] <== guess[k];
+            blow += equalHB[5*j+k].out;
+            if (j == k) {
+                hit += equalHB[5*j+k].out;
+                blow -= equalHB[5*j+k].out;
+            }
+        }
+    }
+
+    // Create a constraint around the number of hit
+    component equalHit = IsEqual();
+    equalHit.in[0] <== pubNumHit;
+    equalHit.in[1] <== hit;
+    equalHit.out === 1;
+    
+    // Create a constraint around the number of blow
+    component equalBlow = IsEqual();
+    equalBlow.in[0] <== pubNumBlow;
+    equalBlow.in[1] <== blow;
+    equalBlow.out === 1;
+
+    // Verify that the hash of the private solution matches pubSolnHash
+    component poseidon = Poseidon(6);
+    poseidon.inputs[0] <== privSalt;
+    poseidon.inputs[1] <== privSolnA;
+    poseidon.inputs[2] <== privSolnB;
+    poseidon.inputs[3] <== privSolnC;
+    poseidon.inputs[4] <== privSolnD;
+    poseidon.inputs[5] <== privSolnE;
+
+    solnHashOut <== poseidon.out;
+    pubSolnHash === solnHashOut;
+ }
+
+// variables inside public ensuring that those variables used in MastermindVariation() are public in nature
+component main {public [pubGuessA, pubGuessB, pubGuessC, pubGuessD, pubGuessE, pubNumHit, pubNumBlow, pubSolnHash]} = MastermindVariation();
--- Part1/test/mastermind-test.js
+++ Part1/test/mastermind-test.js
@@ -1,1 +1,203 @@
 //[assignment] write your own unit test to show that your Mastermind variation circuit is working as expected
+
+const path = require('path');
+const chai = require('chai');
+
+const assert = chai.assert;
+
+const testerWasm = require('circom_tester').wasm;
+const poseidonBuild = require('circomlibjs').buildPoseidon;
+
+const f1Field = require('ffjavascript').F1Field;    // script of file returning a buffer
+const scalar = require('ffjavascript').Scalar;      // script of file for passing a buffer
+
+// return a buffer passed to fromString function
+const retBuffer = new f1Field(scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617"));
+
+describe("Electronic Mastermind(Invicta) Variation test", function () {
+
+    var buff;
+    var poseidon;
+
+    // 1. First test showing all 5 guesses are correct
+    // ...............................................
+
+    it("All 5 guesses are correct i.e. 5 hits and 0 blow", async () => {
+        poseidon = await poseidonBuild();
+        buff = poseidon.F;
+
+        // the solutions are:
+        const privSolnA = 2;
+        const privSolnB = 4;
+        const privSolnC = 6;
+        const privSolnD = 8;
+        const privSolnE = 0;
+
+        // generated random int for salt
+        const privSalt = Math.floor(Math.random()*10**10);
+
+        // computing poseidon hash of the salt and solutions
+        const pubSolnHash = poseidon([privSalt,privSolnA,privSolnB,privSolnC,privSolnD,privSolnE]);
+
+        const circuit = await testerWasm("contracts/circuits/MastermindVariation.circom");
+        await circuit.loadConstraints();
+
+        // guesses made by codebreaker where eveyr guess is correct and hit is 5
+        const INPUT = {
+            "pubGuessA": 2,
+            "pubGuessB": 4,
+            "pubGuessC": 6,
+            "pubGuessD": 8,
+            "pubGuessE": 0,
+            "pubNumHit": 5,
+            "pubNumBlow": 0,
+            "pubSolnHash": buff.toObject(pubSolnHash),
+            "privSolnA": privSolnA,
+            "privSolnB": privSolnB,
+            "privSolnC": privSolnC,
+            "privSolnD": privSolnD,
+            "privSolnE": privSolnE,
+            "privSalt" : privSalt
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        assert(retBuffer.eq(retBuffer.e(witness[0]),retBuffer.e(1)));
+    });
+
+    // 2. Second test showing 4 guesses are correct but 1 guess having wrong positon i.e. 1 blow
+    // .........................................................................................
+
+    it("4 guesses are correct, but 1 wrong position(1 blow) i.e. 3 hits and 1 blow", async () => {
+        poseidon = await poseidonBuild();
+        buff = poseidon.F;
+
+        // the solutions are:
+        const privSolnA = 2;
+        const privSolnB = 4;
+        const privSolnC = 6;
+        const privSolnD = 8;
+        const privSolnE = 0;
+
+        // generated random int for salt
+        const privSalt = Math.floor(Math.random()*10**10);
+
+        // computing poseidon hash of the salt and solutions
+        const pubSolnHash = poseidon([privSalt,privSolnA,privSolnB,privSolnC,privSolnD,privSolnE]);
+
+        const circuit = await testerWasm("contracts/circuits/MastermindVariation.circom");
+        await circuit.loadConstraints();
+
+        // guesses made by codebreaker where eveyr guess is correct and hit is 5
+        const INPUT = {
+            "pubGuessA": 2,
+            "pubGuessB": 4,
+            "pubGuessC": 6,
+            "pubGuessD": 3,
+            "pubGuessE": 8,
+            "pubNumHit": 3,
+            "pubNumBlow": 1,
+            "pubSolnHash": buff.toObject(pubSolnHash),
+            "privSolnA": privSolnA,
+            "privSolnB": privSolnB,
+            "privSolnC": privSolnC,
+            "privSolnD": privSolnD,
+            "privSolnE": privSolnE,
+            "privSalt" : privSalt
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        assert(retBuffer.eq(retBuffer.e(witness[0]),retBuffer.e(1)));
+    });
+    // 3. Third test showing 5 guesses are correct but 2 guess having wrong positon i.e. 2 blow
+    // ........................................................................................
+
+    it("5 guesses are correct, but 2 wrong position(2 blow) i.e. 3 hits and 1 blow", async () => {
+        poseidon = await poseidonBuild();
+        buff = poseidon.F;
+
+        // the solutions are:
+        const privSolnA = 2;
+        const privSolnB = 4;
+        const privSolnC = 6;
+        const privSolnD = 8;
+        const privSolnE = 0;
+
+        // generated random int for salt
+        const privSalt = Math.floor(Math.random()*10**10);
+
+        // computing poseidon hash of the salt and solutions
+        const pubSolnHash = poseidon([privSalt,privSolnA,privSolnB,privSolnC,privSolnD,privSolnE]);
+
+        const circuit = await testerWasm("contracts/circuits/MastermindVariation.circom");
+        await circuit.loadConstraints();
+
+        // guesses made by codebreaker where eveyr guess is correct and hit is 5
+        const INPUT = {
+            "pubGuessA": 2,
+            "pubGuessB": 4,
+            "pubGuessC": 8,
+            "pubGuessD": 6,
+            "pubGuessE": 0,
+            "pubNumHit": 3,
+            "pubNumBlow": 2,
+            "pubSolnHash": buff.toObject(pubSolnHash),
+            "privSolnA": privSolnA,
+            "privSolnB": privSolnB,
+            "privSolnC": privSolnC,
+            "privSolnD": privSolnD,
+            "privSolnE": privSolnE,
+            "privSalt" : privSalt
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        assert(retBuffer.eq(retBuffer.e(witness[0]),retBuffer.e(1)));
+    });
+    // 4. All guesses are incorrect i.e. 0 hit and 0 blow
+    // ..................................................
+
+    it("All 5 guesses are incorrect i.e. 0 hit and 0 blow", async () => {
+        poseidon = await poseidonBuild();
+        buff = poseidon.F;
+
+        // the solutions are:
+        const privSolnA = 2;
+        const privSolnB = 4;
+        const privSolnC = 6;
+        const privSolnD = 8;
+        const privSolnE = 0;
+
+        // generated random int for salt
+        const privSalt = Math.floor(Math.random()*10**10);
+
+        // computing poseidon hash of the salt and solutions
+        const pubSolnHash = poseidon([privSalt,privSolnA,privSolnB,privSolnC,privSolnD,privSolnE]);
+
+        const circuit = await testerWasm("contracts/circuits/MastermindVariation.circom");
+        await circuit.loadConstraints();
+
+        // guesses made by codebreaker where eveyr guess is correct and hit is 5
+        const INPUT = {
+            "pubGuessA": 1,
+            "pubGuessB": 3,
+            "pubGuessC": 5,
+            "pubGuessD": 7,
+            "pubGuessE": 9,
+            "pubNumHit": 0,
+            "pubNumBlow": 0,
+            "pubSolnHash": buff.toObject(pubSolnHash),
+            "privSolnA": privSolnA,
+            "privSolnB": privSolnB,
+            "privSolnC": privSolnC,
+            "privSolnD": privSolnD,
+            "privSolnE": privSolnE,
+            "privSalt" : privSalt
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        assert(retBuffer.eq(retBuffer.e(witness[0]),retBuffer.e(1)));
+    });
+});
